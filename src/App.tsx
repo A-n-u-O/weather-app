9ef4b663e1d572b02455191a1fd72a96aabe4d9f
Anuoluwapo <anuvictor2005@gmail.com>
2024-06-19 23:11:52 +0100

    dependencies installed

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex justify-center items-center bg-gradient-to-br from-sky-400 via-rose-400 to-lime-400 h-screen w-full">
+      Hello World
+    </main>
+  );
+}
+
+export default App;
